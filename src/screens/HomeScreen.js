2a8dec4781971110baab24ba483f379876a007e5
Pablo Leite <pabloleite03@gmail.com>
2024-01-02 16:59:09 -0300

    Add files via upload

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {View, Text, ScrollView, Image} from 'react-native';
 import { StatusBar } from "expo-status-bar";
+import { BellIcon } from "react-native-heroicons/outline"
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
@@ -16,7 +17,13 @@ const HomeScreen = () => {
       className="space-y-6 pt-14">      
 
       <View className="mx-4 flex-row justify-between items-center mb-2">
-      <Image source={require("../../assets/image/avatar.png")} style={{height: hp(5), width: hp(5.5)}} />
+        <Image source={require("../../assets/image/avatar.png")} style={{height: hp(5), width: hp(5.5)}} />
+        <BellIcon color="gray" size={hp(4)} />
+      </View>
+
+      <View className="mx-2 space-y-2 mb-2">
+        <Text style={{fontSize: hp(1.9)}} className="text-neutral-600">Hello, Human!</Text>
+
       </View>
       </ScrollView>
     </View>
